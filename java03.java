027e5383b236b0fdfefe71fad711e85eec8bf317
Mathiyazahgan <mathimathiyazahgan@gmail.com>
2024-06-29 16:37:05 +0530

    Find characters count from given string

---
+++ java03.java
@@ -0,0 +1,15 @@
+// Find characters count from given string
+
+public class java03 {
+    public static void main (String args[]){
+        String s= "helloworld";
+        int count =0;
+        for(int i=0;i<s.length();i++){
+            count++;
+        }
+        System.out.println("The total number of character is "+count );
+
+    }
+
+    
+}
