2bf051088e0c773e138fe3438124c2069fcebc81
Mathiyazahgan <mathimathiyazahgan@gmail.com>
2024-06-29 17:25:43 +0530

    Find given String is a palindrome

---
+++ java04.java
@@ -0,0 +1,20 @@
+//Find given character characters occurrence count from given String
+import java.util.Scanner;
+
+public class java04 {
+    public static void main (String args[]){
+        char s1 ='d';
+    int count =0;
+    String s ="daddy";
+
+    for(int i=0;i<s.length();i++){
+       char ch =s.charAt(i);
+       if(ch==s1){
+        count++;
+       }
+
+    }
+    System.out.println("The character ocurrence in the string is "+count);
+
+}
+}
