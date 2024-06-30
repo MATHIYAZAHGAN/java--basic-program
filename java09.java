3c1f2a105be3b521ed042e89195340efb38d33c3
Mathiyazahgan <mathimathiyazahgan@gmail.com>
2024-06-30 23:49:24 +0530

    Find vowels count from given string

---
+++ java09.java
@@ -0,0 +1,17 @@
+// Find vowels count from given string
+public class java09{
+    public static void main(String[] args) {
+        String s="aeINDIAi";
+        int count=0;
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch=='a'||ch=='A'|| ch=='e'||ch=='E'||ch=='i'||ch=='I'||ch=='o'||ch=='O'||ch=='u'||ch=='U'){
+                count++;
+
+            }
+        }
+        System.out.println("The vowel count is "+count);
+
+    }
+}
+
