dc3625f4f256c6aef6701493de488fef548ae291
Mathiyazahgan <mathimathiyazahgan@gmail.com>
2024-07-03 23:26:01 +0530

    Insert the new element into the array at the designated position

---
+++ java11.java
@@ -0,0 +1,19 @@
+public class java11 {
+    public static void main(String[] args) {
+        int arr[]={10,20,30,40,50};
+        int brr[]=new int[arr.length+1];
+        int n=0;
+        int value=451;
+    
+        for(int i=n;i<arr.length;i++){
+            arr[n]=value;
+
+
+        }
+        for(int i=0;i<arr.length;i++){
+            brr[i]=arr[i];
+            System.out.print(brr[i]+" ");
+        }
+    }
+    
+}
