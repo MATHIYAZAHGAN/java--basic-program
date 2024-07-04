3f67e46d2082b6542c8e4bce338fc44bbf2e0374
Mathiyazahgan <mathimathiyazahgan@gmail.com>
2024-07-04 17:39:54 +0530

    Find Prime number from 0 to 50

---
+++ java15.java
@@ -0,0 +1,15 @@
+//Find Prime number from 0 to 50
+
+public class java15 {
+public static void main (String args[]){
+    int arr[]={10,20,30,40,50};
+    int sum=0;
+    for(int i=0;i<arr.length;i++){
+        sum=sum+arr[i];
+
+    }
+    System.out.println(sum);
+   
+}    
+    
+}
