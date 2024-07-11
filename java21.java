9f26e449d70bd24fa8488d90752c781a6324c740
Mathiyazahgan <mathimathiyazahgan@gmail.com>
2024-07-11 16:15:43 +0530

    find if there is any special characters in a string and remove it

---
+++ java21.java
@@ -0,0 +1,18 @@
+// add
+
+public class java21 {
+    public static void main(String[] args) {
+        String s="lkmngkiin@(*$(oplmwk))";
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(ch != '*' && ch !='.' && ch !='/' && ch != '#' && ch != '@' && ch != '$' && ch != '!'){
+                System.out.print(ch);
+
+            }
+
+        }
+
+        
+    }
+    
+}
