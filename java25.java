0054e5bfb4b961c88d773ee4b4c836d3ac78c11f
Mathiyazahgan <mathimathiyazahgan@gmail.com>
2024-07-12 11:25:24 +0530

    Find square values of 1 to 10

---
+++ java25.java
@@ -0,0 +1,13 @@
+//Find square values of 1 to 10
+
+public class java25 {
+    public static void main(String[] args) {
+        int n=10;
+        for(int i=1;i<=n;i++){
+            int square=i*i;
+gi            System.out.print(square+" ");
+
+        }
+    }
+    
+}
