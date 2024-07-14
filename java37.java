5c851ba1d87231bc489b7c4990be6849fdf6c99d
Mathiyazahgan <mathimathiyazahgan@gmail.com>
2024-07-14 20:34:54 +0530

    Java Program to print all negative elements in an array.

--- java37.java
+++ java37.java
@@ -5,7 +5,7 @@ public class java37 {
         int arr[]={-102,52,-89,45,841,-584};
         for(int i=0;i<arr.length;i++){
             if(arr[i]<0){
-                System.out.print(arr[i]+"  ");
+                System.out.print(arr[i]+"   ");
             }
         }
     }
