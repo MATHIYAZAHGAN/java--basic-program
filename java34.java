6e073e2422fac24f67b78c23c8b6f9ff90f2a4a3
Mathiyazahgan <mathimathiyazahgan@gmail.com>
2024-07-14 20:06:17 +0530

    Java Program to count total number of elements in array.

---
+++ java34.java
@@ -0,0 +1,16 @@
+//Java Program to count total number of elements in array.
+
+
+public class java34 {
+    public static void main(String[] args) {
+        
+    
+    int arr[]={10,20,30,40,50,60,70,80,90};
+    int count=0;
+    for(int i=0;i<arr.length;i++){
+        count++;
+
+    }
+    System.out.println( " The total numer array element is : "+count);
+}
+}
