969916e1bc23a0cdb107f79d8dd5ee1dfcdf2832
Mathiyazahgan <mathimathiyazahgan@gmail.com>
2024-07-14 20:42:30 +0530

    Java Program to find sum of all array elements.

---
+++ java39.java
@@ -0,0 +1,15 @@
+//Java Program to find sum of all array elements.
+
+public class java39 {
+    public static void main(String[] args) {
+        int arr[]={10,20,30,40,50,60,70,80,90,100};
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            sum = sum+arr[i];
+            
+        }
+        System.out.println("The sum of all the array element is :"+sum);
+        
+    }
+    
+}
