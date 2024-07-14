04ee07d8843d5caa322bca3b4d724b0f9b72cc4b
Mathiyazahgan <mathimathiyazahgan@gmail.com>
2024-07-14 18:28:51 +0530

    For the given string find the sum of index

--- java32.java
+++ java32.java
@@ -1,7 +1,13 @@
 // For the given string find the sum of index
+import java.util.*;
 class java32{
     public static void main(String[] args) {
-        String s ="mathiyazahgan";
+        Scanner scan = new Scanner(System.in);
+        while(true){
+            System.out.println();
+        System.out.println("Enter your String!!");
+        String s = scan.nextLine();
+
         int sum=0;
         for(int i=0;i<s.length();i++){
             char ch = s.charAt(i);
@@ -9,6 +15,9 @@ class java32{
             
 
         }
-        System.out.println(sum);
+        System.out.println();
+        System.out.println("***output***");
+        System.out.println("The sum of index value is "+sum);
+    }
     }
 }
