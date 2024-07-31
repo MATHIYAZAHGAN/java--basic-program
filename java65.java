e337a4b9ad5b2dc8557e0c0f1d154e45a3c25b5d
Mathiyazahgan <mathimathiyazahgan@gmail.com>
2024-07-31 22:22:08 +0530

    31-07-2024-3  sum of first 10 natural number.

---
+++ java65.java
@@ -0,0 +1,16 @@
+// 	Create multiplication table Upto 10.
+// Ex:
+// Input is 2.
+
+public class java65 {
+    public static void main (String args[]){
+        int a =2;
+        for(int i=1;i<=10;i++){
+            
+            System.out.println(i+" * 2 = "+i*a);
+        }
+        
+
+    }
+    
+}
