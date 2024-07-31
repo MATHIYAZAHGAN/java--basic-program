bca4431f4d63824b8acdf687e5b98716d03c51c4
Mathiyazahgan <mathimathiyazahgan@gmail.com>
2024-07-31 22:11:58 +0530

    31-07.2024-2 sum of first 10 natural number.

---
+++ java64.java
@@ -0,0 +1,15 @@
+//2.	 sum of first 10 natural number.
+public class java64 {
+    public static void main (String args[]){
+        int a= 10;
+        int sum=0;
+        for(int i=1;i<=a;i++){
+            sum=sum+i;
+
+        }
+        System.out.println("The sum of "+a+" natural number is "+sum);
+
+    }
+
+    
+}
