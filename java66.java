d80dd815f96b794c86b436b6a78f57878140337e
Mathiyazahgan <mathimathiyazahgan@gmail.com>
2024-07-31 22:29:34 +0530

    31-07-2024-4     FACTORIAL NUMBER.

---
+++ java66.java
@@ -0,0 +1,15 @@
+//	FACTORIAL NUMBER.
+public class java66 {
+    public static void main (String args[]){
+        int a=5;
+        int factor=1;
+        for(int i=1;i<=5;i++){
+            factor=factor*i;
+
+
+        }
+        System.out.println(factor);
+
+    }
+    
+}
