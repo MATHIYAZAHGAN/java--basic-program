a3b3e22738ccd71e34b59684dcacb4cad2688e9a
Mathiyazahgan <mathimathiyazahgan@gmail.com>
2024-07-31 22:01:59 +0530

    print numbers from 1 to 10.

---
+++ java63.java
@@ -0,0 +1,13 @@
+//print numbers from 1 to 10.
+
+public class java63 {
+    public static void main (String args[]){
+        int a =10;
+        for(int i=1;i<=a;i++){
+            System.out.print(i+"  ");
+        }
+
+    }
+
+    
+}
